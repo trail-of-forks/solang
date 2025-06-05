61c852f90b9aa3722b13b3b07b1113212101ce24
Samuel Moelius <samuel.moelius@trailofbits.com>
2025-06-05 11:05:01 -0400

    Simplify `Expression:ByteSwap`

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -731,7 +731,6 @@ pub enum Expression {
         pointer: Box<Expression>,
     },
     ByteSwap {
-        ty: Type,
         expr: Box<Expression>,
         le_to_be: bool,
     },
@@ -931,8 +930,7 @@ impl RetrieveType for Expression {
             | Expression::BytesCast { ty, .. }
             | Expression::RationalNumberLiteral { ty, .. }
             | Expression::Subscript { ty, .. }
-            | Expression::InternalFunctionCfg { ty, .. }
-            | Expression::ByteSwap { ty, .. } => ty.clone(),
+            | Expression::InternalFunctionCfg { ty, .. } => ty.clone(),
 
             Expression::BoolLiteral { .. }
             | Expression::MoreEqual { .. }
@@ -948,6 +946,8 @@ impl RetrieveType for Expression {
             Expression::FormatString { .. } => Type::String,
             Expression::VectorData { .. } => Type::Uint(64),
             Expression::Poison => unreachable!("Expression does not have a type"),
+
+            Expression::ByteSwap { expr, .. } => expr.ty(),
         }
     }
 }
