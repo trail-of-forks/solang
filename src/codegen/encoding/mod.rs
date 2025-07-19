trail-of-forks/solang

commit 8329a9de67ed1607116d867943949e23238540fb

Fix encoding of addresses for Stylus

src/codegen/encoding/mod.rs
@@ -233,7 +233,26 @@ pub(crate) trait AbiEncoding {
         let expr_ty = &expr.ty().unwrap_user_type(ns);
         match expr_ty {
             Type::Contract(_) | Type::Address(_) => {
-                self.encode_directly(expr, buffer, offset, vartab, cfg, ns.address_length.into())
+                if ns.target == Target::Stylus {
+                    self.encode_int(
+                        &expr.cast(&Type::Uint(160), ns).cast(&Type::Uint(256), ns),
+                        buffer,
+                        offset,
+                        ns,
+                        vartab,
+                        cfg,
+                        256,
+                    )
+                } else {
+                    self.encode_directly(
+                        expr,
+                        buffer,
+                        offset,
+                        vartab,
+                        cfg,
+                        ns.address_length.into(),
+                    )
+                }
             }
             Type::Bool => self.encode_directly(expr, buffer, offset, vartab, cfg, 1.into()),
             Type::Uint(width) | Type::Int(width) => {
@@ -1567,11 +1586,24 @@ pub(crate) trait AbiEncoding {
                 ty: Uint(32),
                 value: BigInt::from(self.encoding_size(ns, *n) / 8),
             },
-            Type::Enum(_) | Type::Contract(_) | Type::Bool | Type::Address(_) | Type::Bytes(_) => {
-                Expression::NumberLiteral {
-                    loc: Codegen,
-                    ty: Uint(32),
-                    value: ty.memory_size_of(ns),
+            Type::Enum(_) | Type::Bool | Type::Bytes(_) => Expression::NumberLiteral {
+                loc: Codegen,
+                ty: Uint(32),
+                value: ty.memory_size_of(ns),
+            },
+            Type::Contract(_) | Type::Address(_) => {
+                if ns.target == Target::Stylus {
+                    Expression::NumberLiteral {
+                        loc: Codegen,
+                        ty: Uint(32),
+                        value: BigInt::from(256),
+                    }
+                } else {
+                    Expression::NumberLiteral {
+                        loc: Codegen,
+                        ty: Uint(32),
+                        value: ty.memory_size_of(ns),
+                    }
                 }
             }
             Type::FunctionSelector => Expression::NumberLiteral {

commit ef1d25cc5171639a35660913f9929a791c30c9d0

Fix bugs introduced by 8329a9de67ed1607116d867943949e23238540fb

src/codegen/encoding/mod.rs
@@ -903,7 +903,12 @@ pub(crate) trait AbiEncoding {
             | Type::Enum(_)
             | Type::Value
             | Type::Bytes(_) => {
-                let read_bytes = ty.memory_size_of(ns);
+                let (read_ty, read_bytes) =
+                    if ns.target == Target::Stylus && matches!(ty, Type::Address(_)) {
+                        (Type::Uint(256), BigInt::from(32))
+                    } else {
+                        (ty.clone(), ty.memory_size_of(ns))
+                    };
 
                 let size = Expression::NumberLiteral {
                     loc: Codegen,
@@ -912,12 +917,21 @@ pub(crate) trait AbiEncoding {
                 };
                 validator.validate_offset_plus_size(offset, &size, ns, vartab, cfg);
 
-                let read_value = Expression::Builtin {
+                let mut read_value = Expression::Builtin {
                     loc: Codegen,
-                    tys: vec![ty.clone()],
+                    tys: vec![read_ty],
                     kind: Builtin::ReadFromBuffer,
                     args: vec![buffer.clone(), offset.clone()],
                 };
+
+                if ns.target == Target::Stylus && matches!(ty, Type::Address(_)) {
+                    read_value = Expression::ByteSwap {
+                        expr: Box::new(read_value),
+                        le_to_be: false,
+                    };
+                    read_value = read_value.cast(&Type::Uint(160), ns);
+                    read_value = read_value.cast(&ty, ns);
+                }
 
                 let read_var = vartab.temp_anonymous(ty);
                 cfg.add(
@@ -1596,7 +1610,7 @@ pub(crate) trait AbiEncoding {
                     Expression::NumberLiteral {
                         loc: Codegen,
                         ty: Uint(32),
-                        value: BigInt::from(256),
+                        value: BigInt::from(32),
                     }
                 } else {
                     Expression::NumberLiteral {